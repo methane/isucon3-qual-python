c9a7b82966bbd6267892d55be2717690a0ffc73a
INADA Naoki <songofacandy@gmail.com>
2014-09-22 09:26:50 +0000

    Fix some bugs

--- restart.sh
+++ restart.sh
@@ -3,5 +3,5 @@ sudo rm -rf /dev/shm/nginx-cache/*
 sudo rm /var/log/nginx/access.log
 sudo service nginx restart
 sudo supervisorctl restart isucon_python
-sudo supervisorctl restart isucon_python2
+#sudo supervisorctl restart isucon_python2
 curl http://localhost/__init__
